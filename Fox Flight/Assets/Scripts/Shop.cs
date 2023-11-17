32156e3ee4a5e3121a4996b0fc39ea7394a4a6b3
Erika Becker <97013344+eribecke@users.noreply.github.com>
2023-11-16 19:38:28 -0500

    sprite is switching

--- Fox Flight/Assets/Scripts/Shop.cs
+++ Fox Flight/Assets/Scripts/Shop.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
+using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,10 +10,15 @@ using UnityEngine.UI;
 public class Shop : MonoBehaviour
 {
     [SerializeField] Button nextRun;
+    [SerializeField] Button buy;
+    [SerializeField] TextMeshProUGUI buyTxt;
+   
     // Start is called before the first frame update
     void Start()
     {
         nextRun.onClick.AddListener(() => nextRunClicked());
+     
+     
     }
 
     // Update is called once per frame
@@ -24,4 +31,12 @@ public class Shop : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+
+    void buyClicked()
+    {
+  
+    
+        
+    }
+
 }
